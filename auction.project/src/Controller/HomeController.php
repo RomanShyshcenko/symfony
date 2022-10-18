7d7d827b12f0355626bc61019c4cd5cf390348ca
rom <shyshchenkoroman@gmail.com>
2022-10-18 15:00:15 +0300

    fix 403 Forbidden

--- auction.project/src/Controller/HomeController.php
+++ auction.project/src/Controller/HomeController.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Model\User\Entity\User\User;
-use phpDocumentor\Reflection\DocBlock\Tags\Uses;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,7 +11,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-
     /**
      * @Route("/", name="home")
      */
